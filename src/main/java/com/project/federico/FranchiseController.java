cd489ce50696b1ea5a85e25e44c250b2248ca938
gniusk614 <gniusk614@218.38.137.28>
2022-01-24 12:32:21 +0900

    머지를 위한 커밋 로그인창 로고이미지 변경

--- src/main/java/com/project/federico/FranchiseController.java
+++ src/main/java/com/project/federico/FranchiseController.java
@@ -1,19 +1,14 @@
 package com.project.federico;
 
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.log4j.Log4j;
-import paging.PageMaker;
-import paging.SearchCriteria;
 import service.FranchiseService;
-import vo.FranchiseVO;
 
 @RequestMapping(value = "/franchise")
 @Log4j
@@ -25,6 +20,13 @@ public class FranchiseController {
 	@Autowired
 	PasswordEncoder passwordEncoder;
 
+	// 로그인폼이동 (강광훈)
+		@RequestMapping(value = "/loginf")
+		public ModelAndView loginf(ModelAndView mv) {
+			mv.setViewName("franchise/fcLoginForm");
+			return mv;
+		}// loginf-> 폼으로 이동시켜줌
+	
 	
 
 }
--- src/main/webapp/WEB-INF/views/client/clientLoginForm.jsp
+++ src/main/webapp/WEB-INF/views/client/clientLoginForm.jsp
@@ -22,7 +22,7 @@
 	src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script
 	src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
-
+  <script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-{SDK-Version 1.2.0}.js"></script>
 <style>
 .checked {
 	border-bottom: 2px solid black;
