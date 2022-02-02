d45fe7cb6c42d5991a97f80cd6b9ec7a4e45b6c6
gniusk614 <gniusk614@192.168.219.105>
2022-02-02 22:55:26 +0900

    커

--- src/main/java/com/project/federico/ClientController.java
+++ src/main/java/com/project/federico/ClientController.java
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -46,7 +45,6 @@ import vo.MenuVO;
 import vo.NoticeBoardVO;
 import vo.OrderDetailListVO;
 import vo.OrderListVO;
-import vo.StaffVO;
 import vo.ComplainBoardVO;
 
 @RequestMapping(value = "/client")
