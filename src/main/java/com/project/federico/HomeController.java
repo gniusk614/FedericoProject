bcb15e248c42665cd7df8c3ffb4500bffd7c002d
KKKhg <lionskang@google.com>
2022-02-12 01:18:25 +0900

    ubuntu 자바timeZone 설정

--- src/main/java/com/project/federico/HomeController.java
+++ src/main/java/com/project/federico/HomeController.java
@@ -1,12 +1,9 @@
 package com.project.federico;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
-import org.codehaus.plexus.component.annotations.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,13 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-
 	@RequestMapping(value = {"/home","/"})
 	public String home() {
 		
 		return "redirect:client/home";
 	}
 
+	//어플리케이션 첫 구동시 timezone 설정
 	@PostConstruct
 	public void setTIme() {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
